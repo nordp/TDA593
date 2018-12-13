0ed32e1f9b6babef0280d42a89e2808abeb0bf2a
haexa <sne.racheva@gmail.com>
2018-12-13 10:15:46 +0100

    refactored display a little

--- code/simulator/src/user_interface/Display.java
+++ code/simulator/src/user_interface/Display.java
@@ -22,97 +22,118 @@ public class Display {
 	    		"#######################################################################%n" +
 			    "Program started! You can use this terminal to control your robots.%n" +
 			    "See \"help\" for available commands%n" +
-			    "\"exit\" to quit program. %n")
-	    ;
-	    System.out.printf("\"exit\" to quit program. %n");
+			    "\"exit\" to quit program. %n");
     	while (true) {
     		System.out.printf("> ");
 		    String[] parts = input.nextLine().split(" ");
-		    if (parts.length > 0 && "exit".equals(parts[0])) {
-			    System.exit(0);
-		    } else if (parts.length > 0 && "stop".equals(parts[0])) {
-		    	if (parts.length > 1) {
 
-				    // Todo handle invalid id
-		    		int robot = Integer.parseInt(parts[1]);
-				    System.out.printf("Stopping robot %s%n", robot);
-				    operatorInterface.assignAction(robot, new EmergencyInstruction(true));
-			    } else {
-				    System.out.printf("Stopping all robots %n");
-				    // TODO stop all robots
-			    }
-		    } else if (parts.length > 0 && "start".equals(parts[0])) {
-			    if (parts.length > 1) {
-			    	// Todo handle invalid id
-				    int robot = Integer.parseInt(parts[1]);
-				    System.out.printf("Starting robot %s%n", robot);
-				    operatorInterface.assignAction(robot, new EmergencyInstruction(false));
-			    } else {
-				    System.out.printf("Starting all robots%n");
-				    // TODO start all robots
-			    }
-		    } else if (parts.length > 1 && "robot".equals(parts[0])) {
-
-			    // Todo handle invalid id
+		    if(parts.length > 1){
 		    	int robot = Integer.parseInt(parts[1]);
+		    	switch (parts[0]){
+					case "start":
+						// Todo handle invalid id
+						System.out.printf("Starting robot %s%n", robot);
+						operatorInterface.assignAction(robot, new EmergencyInstruction(false));
+						break;
+					case "stop":
+						// Todo handle invalid id
+						System.out.printf("Stopping robot %s%n", robot);
+						operatorInterface.assignAction(robot, new EmergencyInstruction(true));
+						break;
+					case "robot":
+						// Todo handle invalid id
+						System.out.printf("Robot %s selected, \"q\" to deselect%n", robot);
+						while (true) {
+							System.out.printf("Robot %s > ", robot);
+							parts = input.nextLine().split(" ");
 
-			    System.out.printf("Robot %s selected, \"q\" to deselect%n", robot);
-		    	while (true) {
-				    System.out.printf("Robot %s > ", robot);
-				    parts = input.nextLine().split(" ");
-				    if (parts.length > 0 && "q".equals(parts[0])) {
-				    	break;
-				    } else if (parts.length > 0 && "stop".equals(parts[0])) {
-					    System.out.printf("Stopping robot %s%n", robot);
-					    operatorInterface.assignAction(robot, new EmergencyInstruction(true));
-				    } else if (parts.length > 0 && "start".equals(parts[0])) {
-					    System.out.printf("Starting robot %s%n", robot);
-					    operatorInterface.assignAction(robot, new EmergencyInstruction(false));
-				    } else if (parts.length > 0 && ("color".equals(parts[0]) || "colour".equals(parts[0]))) {
-					    System.out.printf("Changing colo(u)r of robot %s%n", robot);
-					    operatorInterface.assignAction(robot, new ChangeColourInstruction());
-				    } else if (parts.length > 0 && "mission".equals(parts[0])) {
-				    	// TODO parse mission
-				    } else if (parts.length > 0 && "exit".equals(parts[0])) {
-					    System.exit(0);
-				    } else if (parts.length > 0 && "help".equals(parts[0])) {
-				    	String[] commands = {
-				    			"exit                                   - Quits application",
-							    "stop                                   - Stops the robot",
-							    "start                                  - Starts the robot",
-							    "mission {strategy} {1,5 4,8 points...} - Sets a mission for the robot",
-							    "map                                    - show the current map",
-							    "q                                      - deselect the robot",
-							    "help                                   - show this help text",
-							    "colour || color                        - makes the robot change colo(u)r",
-				    	};
-					    System.out.printf(String.join("%n", commands) + "%n");
-				    } else if (parts.length > 0 && "map".equals(parts[0])) {
-				    	// TODO print map
-				    } else {
-					    System.out.printf("Input wasn't recognized see \"help\" for available commands %n");
-				    }
-			    }
-		    } else if (parts.length > 0 && "help".equals(parts[0])) {
-			    String[] commands = {
-					    "exit                 - Quits application",
-					    "stop                 - Stops all robots",
-					    "stop  {robotId}      - Stops the robot",
-					    "start                - Starts all robots",
-					    "start {robotId}      - Starts the robot",
-					    "robot {robotId}      - selects the robot",
-					    "robots               - get info about available robots",
-					    "map                  - show the current map",
-					    "help                 - show this help text",
-			    };
-			    System.out.printf(String.join("%n", commands) + "%n");
-		    } else if (parts.length > 0 && "map".equals(parts[0])) {
-			    // TODO print map
-		    } else if (parts.length > 0 && "robots".equals(parts[0])) {
-			    // TODO print robots info
-		    } else {
-			    System.out.printf("Input wasn't recognized see \"help\" for available commands %n");
-		    }
+							if (parts.length > 0) {
+								switch (parts[0]) {
+									case "start":
+										System.out.printf("Starting robot %s%n", robot);
+										operatorInterface.assignAction(robot, new EmergencyInstruction(false));
+										break;
+									case "stop":
+										System.out.printf("Stopping robot %s%n", robot);
+										operatorInterface.assignAction(robot, new EmergencyInstruction(true));
+										break;
+									case "color":
+									case "colour":
+										System.out.printf("Changing colo(u)r of robot %s%n", robot);
+										operatorInterface.assignAction(robot, new ChangeColourInstruction());
+										break;
+									case "mission":
+										//TODO: parse mission
+										break;
+									case "exit":
+										System.exit(0);
+										break;
+									case "help":
+										String[] commands = {
+												"exit                                   - Quits application",
+												"stop                                   - Stops the robot",
+												"start                                  - Starts the robot",
+												"mission {strategy} {1,5 4,8 points...} - Sets a mission for the robot",
+												"map                                    - show the current map",
+												"q                                      - deselect the robot",
+												"help                                   - show this help text",
+												"colour || color                        - makes the robot change colo(u)r",
+										};
+										System.out.printf(String.join("%n", commands) + "%n");
+										break;
+									case "q":
+										break;
+									default:
+										System.out.printf("Input wasn't recognized see \"help\" for available commands %n");
+								}
+
+								if (parts[0].equals("q"))
+									break;
+								
+							} else {
+								System.out.printf("Input wasn't recognized see \"help\" for available commands %n");
+							}
+						}
+				}
+			}
+		    else if(parts.length > 0){
+				switch (parts[0]){
+					case "start":
+						System.out.printf("Starting all robots%n");
+						// TODO start all robots
+						break;
+					case "stop":
+						System.out.printf("Stopping all robots %n");
+						// TODO stop all robots
+						break;
+					case "map":
+						//TODO: print map?
+						break;
+					case "exit":
+						System.exit(0);
+						break;
+					case "help":
+						String[] commands = {
+								"exit                 - Quits application",
+								"stop                 - Stops all robots",
+								"stop  {robotId}      - Stops the robot",
+								"start                - Starts all robots",
+								"start {robotId}      - Starts the robot",
+								"robot {robotId}      - selects the robot",
+								"robots               - get info about available robots",
+								"map                  - show the current map",
+								"help                 - show this help text",
+						};
+						System.out.printf(String.join("%n", commands) + "%n");
+						break;
+					case "robots":
+						//TODO: print robots info
+						break;
+					default:
+						System.out.printf("Input wasn't recognized see \"help\" for available commands %n");
+				}
+			}
+
 	    }
 
     }

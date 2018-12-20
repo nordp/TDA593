ae14a410bdbc44d1ece7df2bca5a4177b361ad3b
Vroxie <victor.c.johansson@gmail.com>
2018-12-20 11:22:04 +0100

    Added test of Strategy

---
+++ code/simulator/test/control_station/StrategyTest.java
@@ -0,0 +1,50 @@
+package control_station;
+
+import model.Coordinate;
+import model.Mission;
+import model.Strategy;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+public class StrategyTest {
+
+    private Coordinate c1 = new Coordinate(1,1);
+    private Coordinate c2 = new Coordinate(2,2);
+    private Coordinate c3 = new Coordinate(3,3);
+    private Coordinate c4 = new Coordinate(4,4);
+
+    @Test
+    public void testStrategizer(){
+        Strategy strategy1 = Strategy.GIVEN_ORDER;
+        Strategy strategy2 = Strategy.BACKWARDS;
+        List<Coordinate> coordinates = new ArrayList<>();
+        coordinates.add(c1);
+        coordinates.add(c2);
+        coordinates.add(c3);
+        coordinates.add(c4);
+
+        Mission mission = new Mission(1,coordinates);
+        Mission missiongiven = Strategizer.strategize(strategy1,mission);
+        assertEquals(mission,missiongiven);
+
+        Mission missionbackward = Strategizer.strategize(strategy2,mission);
+        boolean isSameCoord = true;
+        List<Coordinate> backwardcoordinates = missionbackward.getPoints();
+        Collections.reverse(backwardcoordinates);
+        for(int i = 0;i<backwardcoordinates.size();i++){
+            if(!(backwardcoordinates.get(i).equals(coordinates.get(i)))){
+                isSameCoord = false;
+            }
+        }
+
+        assertTrue(isSameCoord);
+    }
+}
